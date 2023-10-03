4a1f61fde14a7e707403929fb1b23d714178c97b
markohara98@gmail.com <markohara98@gmail.com>
2023-10-03 18:23:11 +0100

    make app a placeholder

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import './App.css';
 
 import { HeaderInfo } from './Components/Header/HeaderInfo';
 export const App = () => {
-  const downloadCV = () => {
-    const a = document.createElement('a');
-    a.href = 'path_to_your_cv.pdf'; // Provide the exact path to the CV file
-    a.download = 'MyCV.pdf'; // Name of the downloaded file
-    a.click();
-  };
-
-  const hireMe = () => {
-    alert('Hire me button clicked!');
-  };
-
   return (
-    <HeaderInfo
-      title="Hello, I'm Mark"
-      paragraph="Welcome to my personal website. Here, you can find all information about me."
-      onHireMeClick={hireMe}
-      onDownloadClick={downloadCV}
-    />
+    <div className="background">
+      <div className="content">
+        <h1>Our Website is Under Construction</h1>
+        <p>Stay tuned for something amazing!</p>
+      </div>
+    </div>
   );
 };
